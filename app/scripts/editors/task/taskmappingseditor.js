2c336e0444dc00e3948620270e4c2076d1dad311
Thijs Petter <thijspetter@gmail.com>
2019-12-18 11:53:23 +0100

    Calculate proper size for task parameter mapping form size
    Based on the number of input and output mappings in the task.

--- app/scripts/editors/task/taskmappingseditor.js
+++ app/scripts/editors/task/taskmappingseditor.js
@@ -21,10 +21,24 @@ class TaskMappingsEditor extends StandardForm {
     </div>
 </div>`);
         const inputMappingsContainer = this.htmlContainer.find('.input-mappings .containerbox');
+        const numInputMappings = this.task.planItemDefinition.inputMappings.length;
+        const numOutputMappings = this.task.planItemDefinition.outputMappings.length;
+        const rowHeight = 32;
+        const h4height = 100;
+        // Input and output height are determined by the number of parameters of the task
+        const inputHeight = rowHeight * numInputMappings + h4height;
+        const outputHeight = rowHeight * numOutputMappings + h4height;
+        // Control height is determined by the mappings (+ 1 extra) plus the header menu bar of the form.
+        const controlHeight = inputHeight + outputHeight + 2 * rowHeight + 20;
+        console.log("I: "+inputHeight+", O: "+outputHeight + ", C: "+controlHeight)
         this.inputMappings = new InputMappingControl(this, inputMappingsContainer);
         const outputMappingsContainer = this.htmlContainer.find('.output-mappings .containerbox');
         this.outputMappings = new OutputMappingControl(this, outputMappingsContainer);
-        this.splitter = new BottomSplitter(this.htmlContainer.find('.task-mappings'), '50%', 100);
+        inputMappingsContainer.css('height', inputHeight + 'px');
+        outputMappingsContainer.css('height', outputHeight + 'px');
+        this.htmlContainer.css('height', controlHeight + 'px');
+        this.htmlContainer.parent().css('height', (controlHeight + 20) +'px');
+        this.splitter = new BottomSplitter(this.htmlContainer.find('.task-mappings'), inputHeight+'px', 100);
         this.splitter.bar.addClass('separator task-mappings-separator');
     }
 
