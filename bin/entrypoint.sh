50e3efcb666ce77243c4d4a3dc8eb19eb0ee909d
suzannevandergrind <suzanne.vandergrind@spronq.com>
2021-01-28 17:31:05 +0100

    copy default models

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,6 +4,10 @@ NODE_ENV=docker
 
 export $NODE_ENV
 
+echo "copy default to repository"
+cp /opt/repository_default/* /opt/repository
+cp /opt/repository_deploy_default/* /opt/repository_deploy
+
 echo "Start repository service"
 /usr/src/app/bin/www &
 
